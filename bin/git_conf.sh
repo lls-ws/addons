cdaf9842065e26b77c5c3f96b672b44effc01602
Leandro Luiz <69089565+lls-ws@users.noreply.github.com>
2020-08-04 01:00:52 -0300

    Add files via upload

---
+++ bin/git_conf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Script para gerenciar os repositorios no Github
+#
+# email: lls.homeoffice@gmail.com
+
+git_update()
+{
+	
+	# Updating a local repository with changes from a GitHub repository
+	git pull origin master
+	
+}
+
+if [ "$EUID" -ne 0 ]; then
+	echo "Rodar script como root"
+	exit 1
+  
+fi
+
+case "$OPCAO" in
+	update)    	
+		git_update
+		;; 
+	*)
+		echo "Use: $(basename $0) {update}"
+		exit 1
+		;;
+esac
