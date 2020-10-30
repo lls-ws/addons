8b484b1bf9a448ae65bf0ed3acbfa70181239825
Leandro Luiz <69089565+lls-ws@users.noreply.github.com>
2020-10-30 05:11:09 -0300

    Add files via upload

--- bin/ssl_conf.sh
+++ bin/ssl_conf.sh
@@ -6,7 +6,7 @@
 
 # Caminho das bibliotecas
 PATH=.:$(dirname $0):$PATH
-. lib/tomcat.lib		|| exit 1
+. cloud/lib/tomcat.lib		|| exit 1
 
 create_pfx()
 {
@@ -46,6 +46,44 @@ show_pfx()
 	
 }
 
+certbot()
+{
+	
+	echo " -- Cleaning -- "
+	sudo rm -fv request.csr
+	sudo rm -fv *.pem
+
+	echo " -- Stop Services -- "
+	sudo iptables -F -t nat
+	sudo service tomcat stop
+
+	echo " -- Delete Keystore -- "
+	sudo rm -fv ${KEYSTORE}
+
+	echo " -- Recreate Keystore -- "
+	sudo keytool -genkey -noprompt -alias ${ALIAS} -dname "CN=${DNAME}, OU=${USER}, O=${USER}, L=Uberlandia, S=MG, C=BR" -keystore ${KEYSTORE} -storepass "${PASSWORD}" -KeySize 2048 -keypass "${PASSWORD}" -keyalg RSA
+
+	echo " -- Build CSR -- "
+	sudo keytool -certreq -alias ${ALIAS} -file request.csr -keystore ${KEYSTORE} -storepass "${PASSWORD}"
+
+	echo " -- Request Certificate -- "
+	sudo certbot certonly --csr ./request.csr --standalone
+
+	echo " -- import Certificate -- "
+	sudo keytool -import -trustcacerts -alias ${ALIAS} -file 0001_chain.pem -keystore ${KEYSTORE} -storepass "${PASSWORD}"
+
+	echo " -- Restart services -- "
+	sudo service tomcat start
+	sudo iptables-restore -n < /etc/iptables/rules.v4
+
+	echo " -- Cleaning -- "
+	sudo rm -fv request.csr
+	sudo rm -fv *.pem
+	
+	show_pfx
+	
+}
+
 case "$1" in
 	create)
 		create_pfx
@@ -53,8 +91,11 @@ case "$1" in
 	show)
 		show_pfx
 		;;
+	certbot)
+		certbot
+		;;
 	*)
-		echo "Use: $0 {create|show}"
+		echo "Use: $0 {create|show|certbot}"
 		exit 1
 		;;
 esac
